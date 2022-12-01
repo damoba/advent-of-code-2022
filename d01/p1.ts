6ba8a499445302f084700171e8b97ca2ddb4cba3
David Moreno-Bautista <davmoba4@gmail.com>
2022-12-01 01:06:13 -0500

    fix: day 01 use constant for array

--- d01/p1.ts
+++ d01/p1.ts
@@ -4,7 +4,7 @@ const elves = (await Deno.readTextFile("./input.txt"))
   .split("\r\n\r\n")
   .map((listStr) => listStr.split("\r\n").map((itemCalsStr) => parseInt(itemCalsStr, 10)));
 
-let maxCalsPerElf: number[] = [];
+const maxCalsPerElf: number[] = [];
 
 elves.forEach((list) => {
   maxCalsPerElf.push(list.reduce((itemCals, c) => itemCals + c, 0));
