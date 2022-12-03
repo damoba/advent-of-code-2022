13ba6162cbb0148d1b2c75565c60975162f4a41a
David Moreno-Bautista <davmoba4@gmail.com>
2022-12-03 02:11:37 -0500

    feat: complete day 03

--- d03/p2.ts
+++ d03/p2.ts
@@ -7,21 +7,24 @@ const capitalPriorityStart = "A".charCodeAt(0) - 27;
 
 let totalPriority = 0;
 
-list.forEach((line) => {
-  const items = new Set();
+let i = 0;
+while (i < list.length) {
+  const itemsPerTrio = [new Set(), new Set()];
   let common = "";
 
-  let i = 0;
-  while (i < line.length / 2) {
-    items.add(line[i]);
-    i += 1;
-  }
-  while (i < line.length) {
-    if (items.has(line[i])) {
-      common = line[i];
-      break;
+  let j = 0;
+  while (j < 2) {
+    for (const item of list[i]) {
+      itemsPerTrio[j].add(item);
     }
     i += 1;
+    j += 1;
+  }
+  for (const item of list[i]) {
+    if (itemsPerTrio[0].has(item) && itemsPerTrio[1].has(item)) {
+      common = item;
+      break;
+    }
   }
 
   if (common === common.toLowerCase()) {
@@ -29,6 +32,8 @@ list.forEach((line) => {
   } else {
     totalPriority += common.charCodeAt(0) - capitalPriorityStart;
   }
-});
+
+  i += 1
+}
 
 console.log("total priority", totalPriority);
