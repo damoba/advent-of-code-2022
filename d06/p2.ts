a6fee99f7a2d3c6d22074d9dd6f087d7322141b7
David Moreno-Bautista <davmoba4@gmail.com>
2022-12-06 20:45:16 -0500

    perf: made day 06 more efficient

--- d06/p2.ts
+++ d06/p2.ts
@@ -3,35 +3,56 @@ import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
 const markerLength = 14;
 
 const solve = (data: string) => {
-  const quartetArr = [];
-  const quartetMap: Map<string, number> = new Map();
-  let hasGreaterThanOne = 0;
+  const quartetArr = []; // window holding the last "markerLength" chars
+  const quartetMap: Map<string, number> = new Map(); // char -> count for window
+  let charsAppearingMoreThanOnce = 0; // count of chars appearing more than once in window
 
   let i = 0;
-  while (i < data.length) {
+  // initialize window with the last char missing, awaiting uniqueness check
+  while (i < markerLength - 1) {
     quartetArr.push(data[i]);
     quartetMap.get(data[i])
       ? quartetMap.set(data[i], (quartetMap.get(data[i]) as number) + 1)
       : quartetMap.set(data[i], 1);
 
+    // if the char is already in the map, it's appearing more than once.
+    // (if the count was higher than 2, it's already been counted)
     if ((quartetMap.get(data[i]) as number) === 2) {
-      hasGreaterThanOne++;
+      charsAppearingMoreThanOnce++;
     }
 
-    if (quartetArr.length == markerLength && hasGreaterThanOne === 0) {
-      break;
-    } else if (quartetArr.length == markerLength) {
-      quartetMap.set(
-        quartetArr[0],
-        (quartetMap.get(quartetArr[0]) as number) - 1
-      );
-      if ((quartetMap.get(quartetArr[0]) as number) === 1) {
-        hasGreaterThanOne--;
-      } else if ((quartetMap.get(quartetArr[0]) as number) === 0) {
-        quartetMap.delete(quartetArr[0]);
-      }
-      quartetArr.shift();
+    i++;
+  }
+
+  while (i < data.length) {
+    // add the last char to the window and update the map
+    quartetArr.push(data[i]);
+    quartetMap.get(data[i])
+      ? quartetMap.set(data[i], (quartetMap.get(data[i]) as number) + 1)
+      : quartetMap.set(data[i], 1);
+
+    // if the char is already in the map, it's appearing more than once.
+    // (if the count was higher than 2, it's already been counted)
+    if ((quartetMap.get(data[i]) as number) === 2) {
+      charsAppearingMoreThanOnce++;
     }
+
+    // if the window is full and there are no chars appearing more than once, we're done
+    if (charsAppearingMoreThanOnce === 0) {
+      break;
+    }
+
+    // otherwise, remove the first char from the window and update the map
+    quartetMap.set(
+      quartetArr[0],
+      (quartetMap.get(quartetArr[0]) as number) - 1
+    );
+    if ((quartetMap.get(quartetArr[0]) as number) === 1) {
+      charsAppearingMoreThanOnce--;
+    } else if ((quartetMap.get(quartetArr[0]) as number) === 0) {
+      quartetMap.delete(quartetArr[0]);
+    }
+    quartetArr.shift();
 
     i++;
   }
