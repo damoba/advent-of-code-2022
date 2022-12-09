8a524b6b6b2b95e94d2de31c56749d30c2fab41f
David Moreno-Bautista <davmoba4@gmail.com>
2022-12-08 19:38:30 -0500

    feat: complete day 08

---
+++ d08/p1.ts
@@ -0,0 +1,98 @@
+import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+
+type Tree = {
+  index: string;
+  num: number;
+};
+
+const calcVisibleTrees = (
+  grid: Tree[][],
+  visibleTrees: Tree[],
+  row: number,
+  col: number,
+  highestTree: Tree,
+  direction: string
+) => {
+  if (
+    row < 1 ||
+    row >= grid.length - 1 ||
+    col < 1 ||
+    col >= grid[0].length - 1
+  ) {
+    return;
+  }
+
+  if (grid[row][col].num > highestTree.num) {
+    highestTree = grid[row][col];
+    if (!visibleTrees.includes(grid[row][col])) {
+      visibleTrees.push(grid[row][col]);
+    }
+  }
+
+  if (direction === "down") {
+    calcVisibleTrees(grid, visibleTrees, row + 1, col, highestTree, "down");
+  } else if (direction === "up") {
+    calcVisibleTrees(grid, visibleTrees, row - 1, col, highestTree, "up");
+  } else if (direction === "right") {
+    calcVisibleTrees(grid, visibleTrees, row, col + 1, highestTree, "right");
+  } else if (direction === "left") {
+    calcVisibleTrees(grid, visibleTrees, row, col - 1, highestTree, "left");
+  }
+};
+
+const solve = (data: string) => {
+  const grid = data.split("\n").map((line, row) =>
+    line.split("").map((treeStr: string, col) => {
+      const tree: Tree = {
+        index: row.toString(10) + "-" + col.toString(10),
+        num: parseInt(treeStr, 10),
+      };
+      return tree;
+    })
+  );
+
+  const ROWS = grid.length;
+  const COLS = grid[0].length;
+  const visibleTrees: Tree[] = [];
+
+  for (let row = 0; row < ROWS; row++) {
+    visibleTrees.push(grid[row][0]);
+    visibleTrees.push(grid[row][COLS - 1]);
+  }
+  for (let col = 1; col < COLS - 1; col++) {
+    calcVisibleTrees(grid, visibleTrees, 1, col, grid[0][col], "down");
+    calcVisibleTrees(
+      grid,
+      visibleTrees,
+      ROWS - 2,
+      col,
+      grid[ROWS - 1][col],
+      "up"
+    );
+  }
+
+  for (let col = 1; col < COLS - 1; col++) {
+    visibleTrees.push(grid[0][col]);
+    visibleTrees.push(grid[ROWS - 1][col]);
+  }
+  for (let row = 1; row < ROWS - 1; row++) {
+    calcVisibleTrees(grid, visibleTrees, row, 1, grid[row][0], "right");
+    calcVisibleTrees(
+      grid,
+      visibleTrees,
+      row,
+      COLS - 2,
+      grid[row][COLS - 1],
+      "left"
+    );
+  }
+
+  return visibleTrees.length;
+};
+
+const example = await Deno.readTextFile("./example.txt");
+const input = await Deno.readTextFile("./input.txt");
+Deno.test("Test and Solve", () => {
+  assertEquals(solve(example), 21);
+  console.log("SOLUTION", solve(input));
+});
---
+++ d08/p2.ts
@@ -0,0 +1,123 @@
+import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+
+const calcViewingDistance = (
+  grid: number[][],
+  row: number,
+  col: number,
+  originalTree: number,
+  countOfShorterTrees: { dist: number },
+  direction: string
+) => {
+  if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
+    return;
+  } else if (grid[row][col] >= originalTree) {
+    countOfShorterTrees.dist++;
+    return;
+  }
+
+  countOfShorterTrees.dist++;
+  if (direction === "down") {
+    calcViewingDistance(
+      grid,
+      row + 1,
+      col,
+      originalTree,
+      countOfShorterTrees,
+      "down"
+    );
+  } else if (direction === "up") {
+    calcViewingDistance(
+      grid,
+      row - 1,
+      col,
+      originalTree,
+      countOfShorterTrees,
+      "up"
+    );
+  } else if (direction === "right") {
+    calcViewingDistance(
+      grid,
+      row,
+      col + 1,
+      originalTree,
+      countOfShorterTrees,
+      "right"
+    );
+  } else if (direction === "left") {
+    calcViewingDistance(
+      grid,
+      row,
+      col - 1,
+      originalTree,
+      countOfShorterTrees,
+      "left"
+    );
+  }
+};
+
+const solve = (data: string) => {
+  const grid = data
+    .split("\n")
+    .map((line) =>
+      line.split("").map((treeStr: string) => parseInt(treeStr, 10))
+    );
+
+  const ROWS = grid.length;
+  const COLS = grid[0].length;
+  let highestScore = 1;
+
+  for (let row = 1; row < ROWS - 1; row++) {
+    for (let col = 1; col < COLS - 1; col++) {
+      const [distanceDown, distanceUp, distanceRight, distanceLeft] = [
+        { dist: 0 },
+        { dist: 0 },
+        { dist: 0 },
+        { dist: 0 },
+      ];
+
+      calcViewingDistance(
+        grid,
+        row + 1,
+        col,
+        grid[row][col],
+        distanceDown,
+        "down"
+      );
+      calcViewingDistance(grid, row - 1, col, grid[row][col], distanceUp, "up");
+      calcViewingDistance(
+        grid,
+        row,
+        col + 1,
+        grid[row][col],
+        distanceRight,
+        "right"
+      );
+      calcViewingDistance(
+        grid,
+        row,
+        col - 1,
+        grid[row][col],
+        distanceLeft,
+        "left"
+      );
+
+      const score =
+        distanceDown.dist *
+        distanceUp.dist *
+        distanceRight.dist *
+        distanceLeft.dist;
+      if (score > highestScore) {
+        highestScore = score;
+      }
+    }
+  }
+
+  return highestScore;
+};
+
+const example = await Deno.readTextFile("./example.txt");
+const input = await Deno.readTextFile("./input.txt");
+Deno.test("Test and Solve", () => {
+  assertEquals(solve(example), 8);
+  console.log("SOLUTION", solve(input));
+});
