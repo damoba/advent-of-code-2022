59b2e94879431259005d951facfbf190a811d6d4
David Moreno-Bautista <davmoba4@gmail.com>
2022-12-15 01:48:52 -0500

    feat: complete day 14

---
+++ d14/p1.ts
@@ -0,0 +1,147 @@
+import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+
+type coordinate = { r: number; c: number };
+type status = { infinity: boolean; addSand: boolean };
+
+const parseScan = (data: string) => {
+  let minC = 500;
+  let maxC = 500;
+  let minR = 0;
+  let maxR = 0;
+
+  const lines = data
+    .split("\n")
+    .map((line) =>
+      line.split("->").map((coor) =>
+        coor.split(",").map((numStr, i) => {
+          const num = parseInt(numStr.trim());
+          if (i === 0) {
+            if (num < minC) {
+              minC = num;
+            }
+            if (num > maxC) {
+              maxC = num;
+            }
+          } else {
+            if (num < minR) {
+              minR = num;
+            }
+            if (num > maxR) {
+              maxR = num;
+            }
+          }
+          return num;
+        })
+      )
+    )
+    .map((line) =>
+      line.map((coor) =>
+        coor.map((num, i) => (i === 0 ? num - minC : num - minR))
+      )
+    );
+
+  const grid = new Array(maxR - minR + 1)
+    .fill(0)
+    .map(() => Array(maxC - minC + 1).fill("."));
+
+  grid[0][500 - minC] = "+";
+  lines.forEach((line) => {
+    for (let i = 1; i < line.length; i++) {
+      if (line[i][1] === line[i - 1][1]) {
+        if (line[i][0] - line[i - 1][0] > 0) {
+          for (let j = 0; j <= line[i][0] - line[i - 1][0]; j++) {
+            grid[line[i][1]][line[i - 1][0] + j] = "#";
+          }
+        } else {
+          for (let j = 0; j <= line[i - 1][0] - line[i][0]; j++) {
+            grid[line[i][1]][line[i - 1][0] - j] = "#";
+          }
+        }
+      } else {
+        if (line[i][1] - line[i - 1][1] > 0) {
+          for (let j = 0; j <= line[i][1] - line[i - 1][1]; j++) {
+            grid[line[i - 1][1] + j][line[i][0]] = "#";
+          }
+        } else {
+          for (let j = 0; j <= line[i - 1][1] - line[i][1]; j++) {
+            grid[line[i - 1][1] - j][line[i][0]] = "#";
+          }
+        }
+      }
+    }
+  });
+
+  return [grid, { r: 0, c: 500 - minC }];
+};
+
+const fall = (grid: string[][], source: coordinate, status: status) => {
+  while (source.r < grid.length && grid[source.r][source.c] === ".") {
+    source.r += 1;
+  }
+  source.r -= 1; // undo the last step
+  // if you went too far down, we got to infinity
+  if (source.r === grid.length - 1) {
+    status.infinity = true;
+    status.addSand = false;
+    return;
+  }
+  if (source.c - 1 >= -1) {
+    // if you went too far left, we got to infinity
+    if (source.c === 0) {
+      status.infinity = true;
+      status.addSand = false;
+      return;
+    }
+    // go left first if you can
+    else if (grid[source.r + 1][source.c - 1] === ".") {
+      fall(grid, { ...source, r: source.r + 1, c: source.c - 1 }, status);
+    }
+  }
+  // if you can't go left, check if you can go right
+  if (
+    grid[source.r + 1][source.c - 1] !== "." &&
+    source.c + 1 < grid[0].length
+  ) {
+    // if you went too far right, we got to infinity
+    if (source.c === grid[0].length - 1) {
+      status.infinity = true;
+      status.addSand = false;
+      return;
+    }
+    // if you can go right, go right
+    else if (grid[source.r + 1][source.c + 1] === ".") {
+      fall(grid, { ...source, r: source.r + 1, c: source.c + 1 }, status);
+    }
+  }
+  // if you're stuck, fill the space (only if you are at the end of the path)
+  if (status.addSand) {
+    grid[source.r][source.c] = "o";
+    status.addSand = false;
+  }
+};
+
+const solve = (data: string) => {
+  const dataParsed = parseScan(data);
+  const grid: string[][] = dataParsed[0] as string[][];
+  const source: coordinate = dataParsed[1] as coordinate;
+  source.r += 1; // start falling
+
+  const status: status = { infinity: false, addSand: true };
+  let count = 0;
+  while (!status.infinity) {
+    status.addSand = true;
+    fall(grid, source, status);
+    if (!status.infinity) {
+      count += 1;
+    }
+  }
+
+  return count;
+};
+
+const example = await Deno.readTextFile("./example.txt");
+const input = await Deno.readTextFile("./input.txt");
+Deno.test("Test and Solve", () => {
+  assertEquals(solve(example), 24);
+  console.log("SOLUTION", solve(input));
+});
---
+++ d14/p2.ts
@@ -0,0 +1,159 @@
+import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+
+type coordinate = { r: number; c: number };
+type status = { addSand: boolean };
+
+const parseScan = (data: string) => {
+  let minC = 500;
+  let maxC = 500;
+  let minR = 0;
+  let maxR = 0;
+
+  const lines = data
+    .split("\n")
+    .map((line) =>
+      line.split("->").map((coor) =>
+        coor.split(",").map((numStr, i) => {
+          const num = parseInt(numStr.trim());
+          if (i === 0) {
+            if (num < minC) {
+              minC = num;
+            }
+            if (num > maxC) {
+              maxC = num;
+            }
+          } else {
+            if (num < minR) {
+              minR = num;
+            }
+            if (num > maxR) {
+              maxR = num;
+            }
+          }
+          return num;
+        })
+      )
+    )
+    .map((line) =>
+      line.map((coor) =>
+        coor.map((num, i) => (i === 0 ? num - minC : num - minR))
+      )
+    );
+
+  const grid = new Array(maxR - minR + 3)
+    .fill(0)
+    .map(() => Array(maxC - minC + 1).fill("."));
+
+  grid[0][500 - minC] = "+";
+  for (let j = 0; j < grid[0].length; j++) {
+    grid[grid.length - 1][j] = "#";
+  }
+  lines.forEach((line) => {
+    for (let i = 1; i < line.length; i++) {
+      if (line[i][1] === line[i - 1][1]) {
+        if (line[i][0] - line[i - 1][0] > 0) {
+          for (let j = 0; j <= line[i][0] - line[i - 1][0]; j++) {
+            grid[line[i][1]][line[i - 1][0] + j] = "#";
+          }
+        } else {
+          for (let j = 0; j <= line[i - 1][0] - line[i][0]; j++) {
+            grid[line[i][1]][line[i - 1][0] - j] = "#";
+          }
+        }
+      } else {
+        if (line[i][1] - line[i - 1][1] > 0) {
+          for (let j = 0; j <= line[i][1] - line[i - 1][1]; j++) {
+            grid[line[i - 1][1] + j][line[i][0]] = "#";
+          }
+        } else {
+          for (let j = 0; j <= line[i - 1][1] - line[i][1]; j++) {
+            grid[line[i - 1][1] - j][line[i][0]] = "#";
+          }
+        }
+      }
+    }
+  });
+
+  return [grid, { r: 0, c: 500 - minC }];
+};
+
+const fall = (
+  grid: string[][],
+  source: coordinate,
+  unit: coordinate,
+  status: status
+) => {
+  while (unit.r < grid.length && grid[unit.r][unit.c] === ".") {
+    unit.r += 1;
+  }
+  unit.r -= 1; // undo the last step
+  if (unit.c - 1 >= -1) {
+    // if you went too far left, add a column
+    if (unit.c === 0) {
+      grid.map((row, r) =>
+        r !== grid.length - 1 ? row.splice(0, 0, ".") : row.splice(0, 0, "#")
+      );
+      source.c++;
+      // if you are the end of the path, fill the space
+      if (status.addSand) {
+        grid[grid.length - 2][unit.c] = "o";
+        status.addSand = false;
+      }
+      return;
+    }
+    // go left first if you can
+    else if (grid[unit.r + 1][unit.c - 1] === ".") {
+      fall(grid, source, { ...unit, r: unit.r + 1, c: unit.c - 1 }, status);
+    }
+  }
+  // if you can't go left check if you can go right
+  if (grid[unit.r + 1][unit.c - 1] !== "." && unit.c + 1 <= grid[0].length) {
+    // if you went too far right, add a column
+    if (unit.c === grid[0].length - 1) {
+      grid.map((row, r) =>
+        r !== grid.length - 1 ? row.push(".") : row.push("#")
+      );
+      // if you are the end of the path, fill the space
+      if (status.addSand) {
+        grid[grid.length - 2][unit.c + 1] = "o";
+        status.addSand = false;
+      }
+      return;
+    }
+    // if you can go right, go right
+    else if (grid[unit.r + 1][unit.c + 1] === ".") {
+      fall(grid, source, { ...unit, r: unit.r + 1, c: unit.c + 1 }, status);
+    }
+  }
+  // if you're stuck, fill the space (only if you are at the end of the path)
+  if (status.addSand) {
+    grid[unit.r][unit.c] = "o";
+    status.addSand = false;
+  }
+};
+
+const solve = (data: string) => {
+  const dataParsed = parseScan(data);
+  const grid: string[][] = dataParsed[0] as string[][];
+  const source: coordinate = dataParsed[1] as coordinate;
+  source.r += 1; // start falling
+
+  const status: status = { addSand: true };
+  let count = 0;
+  while (grid[source.r - 1][source.c] !== "o") {
+    status.addSand = true;
+    fall(grid, source, { ...source }, status);
+    if (grid[source.r - 1][source.c] !== "o") {
+      count += 1;
+    }
+  }
+
+  return count;
+};
+
+const example = await Deno.readTextFile("./example.txt");
+const input = await Deno.readTextFile("./input.txt");
+Deno.test("Test and Solve", () => {
+  assertEquals(solve(example), 93);
+  console.log("SOLUTION", solve(input));
+});
